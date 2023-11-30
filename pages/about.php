c7790c7c11a7d06ed1fc4ba42e08b710702c9ccf
cagas0621 <118946491+cagas0621@users.noreply.github.com>
2023-11-30 17:14:32 +0800

    .

--- pages/about.php
+++ pages/about.php
@@ -11,46 +11,12 @@
     </div>
 </section>
 
-<!-- <section class="ftco-section ftco-fullwidth goto-here">
-    <div class="overlay"></div>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12 heading-section text-center ftco-animate mb-5">
-                <h2 class="mb-2">ABOUT THE COMPANY</h2>
-            </div>
-        </div>
-    </div>
-    <div class="container-fluid px-0">
-        <div class="row d-sm-flex text-wrapper align-items-stretch">
-            <div class="one-half mb-md-0 mb-4 img d-flex align-self-stretch" style="background-image: url('<?php bloginfo('template_directory'); ?>/assets/images/about.jpg');"></div>
-            <div class="one-half half-text d-flex justify-content-end align-items-center">
-                <div class="text-inner pt-md-5 pl-md-5">
-                    <div class="row d-flex">
-                        <div class="col-md-12 d-flex align-self-stretch ftco-animate">
-                            <div class="media block-6 services-wrap d-flex">
-                                <div class="media-body pl-4">
-                                    <h4 class="text-justify font-weight-bold" style="color:#0D3166;">CloudCents Corp. provides comprehensive business process outsourcing (BPO) solutions in the field of accounting and virtual agency services. It offers different professional services including but not limited to: Accounting, Finance, Virtual and Executive Assistants and Lead Generation.
-                                    </h4>
-                                    <br>
-                                    <h4 class="text-justify font-weight-bold" style="color:#FDBE34; text-shadow: 2px 2px 4px #fff;">With us as your dependable business partner providing top-notch BPO services, you may grow your business and reach new heights.
-                                    </h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section> -->
 <section class="ftco-section ftco-no-pb ftco-no-pt">
     <div class="container">
         <div class="row">
 
             <div class="col-md-6 wrap-about pl-md-5 ftco-animate py-md-5">
                 <div class="heading-section pt-md-4">
-                    <!-- <h2 class="mb-4">MISSION AND VISION</h2> -->
-
                     <h3 style="font-weight: bold; text-align: center;">Our <span style="color: #0D3166;">MISSION</span></h3>
                     <p>To promote financial stability and independence by providing accessible accounting and business
                         process
