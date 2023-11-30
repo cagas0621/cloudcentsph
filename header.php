bedda320d22166bacda44073a5ba7cc5417eb194
cagas0621 <118946491+cagas0621@users.noreply.github.com>
2023-11-30 15:20:13 +0800

    .

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 <head>
 	<title>CloudCents</title>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
 
 	<?php wp_head(); ?>
 </head>
