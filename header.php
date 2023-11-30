71db19305cd4a97b785f08f3194ecfe249093ad6
cagas0621 <118946491+cagas0621@users.noreply.github.com>
2023-11-30 15:13:29 +0800

    fix header

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
 	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 		<div class="container">
 			<a class="navbar-brand" href="<?php echo home_url(); ?>"><img class="headerlogoimg" src="<?php bloginfo('template_directory'); ?>/assets/images/Picture1.png" alt=""></a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+			<button style="color: black;" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="oi oi-menu"></span> Menu
 			</button>
 
